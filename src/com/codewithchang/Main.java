95a889edd3696810caed0e1bf5160f9c52af80ff
Chang <cnxiong03@gmail.com>
2020-09-08 00:12:54 -0400

    added interest in SavingsAccount

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -7,14 +7,16 @@ public class Main {
     public static void main(String[] args) {
         CheckingAccount myCheck = new CheckingAccount(1234, 5000, "Calvino");
         SavingsAccount myAcct = new SavingsAccount(333, 5000, "Chang", 5);
-        System.out.println(myAcct.getDetails());
+         System.out.println(myAcct.getDetails());
         myAcct.withdraw(100);
         System.out.println(myAcct.getDetails());
         myAcct.deposit(300);
         System.out.println(myAcct.getDetails());
-        System.out.println(myCheck.getDetails());
-        myCheck.deposit(200);
-        myCheck.withdraw(500);
-        System.out.println(myCheck.getDetails());
+//        System.out.println(myCheck.getDetails());
+//        myCheck.deposit(200);
+//        myCheck.withdraw(500);
+//        System.out.println(myCheck.getDetails());
+        myAcct.applyInterest();
+        System.out.println(myAcct.getDetails());
     }
 }
