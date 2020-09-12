e1ef9fcf719ffc15cd3ef33adf4a9286b4890e4e
Chang <cnxiong03@gmail.com>
2020-09-12 19:46:26 -0400

    building DebitCard method

--- src/Bank/DebitCard.java
+++ src/Bank/DebitCard.java
@@ -1,10 +1,27 @@
 package Bank;
 
+import java.util.List;
+
 public class DebitCard {
     private String pin;
     private CheckingAccount primary;
     private String cvv;
     private String cardOwner;
-    private String CardNum;
-    private
+    private String cardNum;
+    private List<SavingsAccount> atmAccounts;
+
+    public DebitCard(String cardNum, String pin, String cvv, String cardOwner, CheckingAccount primary, List atmAccounts) {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.cvv = cvv;
+        this.cardOwner = cardOwner;
+        this.primary = primary;
+        this.atmAccounts = atmAccounts;
+    }
+
+    public void charge(int amt, String pin) {
+        if (this.pin == pin && primary.balance >= amt) {
+
+        }
+    }
 }
