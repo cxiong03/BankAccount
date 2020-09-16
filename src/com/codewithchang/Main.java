1d79e3b36d660c84a1a7d33a7217283ffb264f7c
Chang <cnxiong03@gmail.com>
2020-09-15 23:57:26 -0400

    added DebitCArd to Main

--- src/com/codewithchang/Main.java
+++ src/com/codewithchang/Main.java
@@ -2,11 +2,15 @@ package com.codewithchang;
 
 import Bank.CheckingAccount;
 import Bank.SavingsAccount;
+import Bank.DebitCard;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         CheckingAccount myCheck = new CheckingAccount(1234, 5000, "Calvino");
         SavingsAccount myAcct = new SavingsAccount(333, 5000, "Chang", 5);
+        SavingsAccount savings2 = new SavingsAccount(5430, 4700, "Cole", 3);
          System.out.println(myAcct.getDetails());
         myAcct.withdraw(100);
         System.out.println(myAcct.getDetails());
@@ -18,5 +22,11 @@ public class Main {
         System.out.println(myCheck.getDetails());
         myAcct.applyInterest();
         System.out.println(myAcct.getDetails());
+        List<SavingsAccount> savingslist = new ArrayList<SavingsAccount>();
+        savingslist.add(myAcct);
+        savingslist.add(savings2);
+        DebitCard myCard = new DebitCard("1234 1234 1234 1133", "1234", "233", "Chang", myCheck,
+                new ArrayList<SavingsAccount>(Arrays.asList(myAcct, savings2))
+        );
     }
 }
